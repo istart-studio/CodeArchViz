7799a853b01bb50b57bbf72919384902f0d2373a
dongyan <7632632@qq.com>
2021-09-23 15:29:09 +0800

    1. 新增树

--- scan/main.py
+++ scan/main.py
@@ -6,7 +6,9 @@ from scan.read_source_file import getJavaFiles
 # 扫描包的文件
 # /Users/dongyan/Documents/workplace/history/dongrun_project/dr_strategy 下的所有 *.java 文件
 # source_files = getJavaFiles("/Users/dongyan/Documents/workplace/history/dongrun_project/dr_strategy/strategy/src/main/java/com/dr/strategy/setting")
-source_files = getJavaFiles("/Users/dongyan/Documents/workplace/history/dongrun_project/dr_strategy")
+# source_files = getJavaFiles("/Users/dongyan/Documents/workplace/history/dongrun_project/dr_strategy")
+file_path = "/Users/dongyan/Documents/workplace/history/dongrun_project/pro_drs"
+source_files = getJavaFiles(file_path);
 
 cards: [] = []
 
@@ -16,5 +18,5 @@ for source_file in source_files:
 cards_json = json.dumps(cards)
 print(cards_json)
 
-with open('/Users/dongyan/Downloads/java_fan_in_fan_out.json', 'w', encoding='utf-8') as f:
+with open('/Users/dongyan/Downloads/java_fan_in_fan_out_drs.json', 'w', encoding='utf-8') as f:
     f.write(cards_json)

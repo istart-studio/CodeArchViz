7452c81363822ac5d268151c3d602c6f00452763
dy <dong-_-yan@163.com>
2022-03-31 09:19:15 +0800

    master 自动生成界面所需的原始数据js

--- scan/main.py
+++ scan/main.py
@@ -18,11 +18,11 @@ for source_file in source_files:
     cards.append(load_source_file(source_file))
 
 cards_json = json.dumps(cards)
-print(cards_json)
+# print(cards_json)
 
-with open('/Users/dy/Downloads/java_fan_in_fan_out.json', 'w', encoding='utf-8') as f:
-    f.write(cards_json)
+# with open('/Users/dy/Downloads/java_fan_in_fan_out.json', 'w', encoding='utf-8') as f:
+#     f.write(cards_json)
 
 js_data = "var data=" + cards_json + ";"
-with open('./view_ec/js/source_json_data.js', 'w', encoding='utf-8') as f:
-    f.write(cards_json)
+with open('view_ec/source_json_data.js', 'w', encoding='utf-8') as f:
+    f.write(js_data)
